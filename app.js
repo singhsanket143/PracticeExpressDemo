5859ddc2244164146b81b98d58d64ae487ad4504
singhsanket143 <singhsanket143@gmail.com>
2022-01-20 22:39:55 +0530

    Added req body parsing

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const apiRouter = require('./routes/api/v1/index');
 const app = express();
 const port = 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use('/api/v1', apiRouter);
 
